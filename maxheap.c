7c9fb2408924e2e3d0e5b62465933a5aff4fa494
rathod1235 <rathodveerender25.vr@gmail.com>
2017-04-29 16:22:21 +0530

    error

--- maxheap.c
+++ maxheap.c
@@ -1,20 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
-void maxHeap(int *array,int n,int k){
-	int i,parent,max=0;
-	for(i=(n-1)/k;i<=0;i--){
-		if(
-
-
-
+void swap(int *array, int i, int j)
+{
+	int temp;
+	temp = array[i];
+	array[i] =array[j];
+	array[j] = temp;
+}
+void restoreDown(int *array,int n,int k,int i){
+	int j,child[k];
+while(1){
+	for(j=0;i<k;i++){
+		if((k*i)+j<n){
+			child[j]=(k*i)+j;
+		}else{
+			child[j]=-1;
+		}
+	}
+	int maxChild=-1,maxChildIndex;
+	for(j=0;j<k;j++){
+		if(child[j]!=-1 && array[child[j]]>maxChild){
+			maxChild=array[child[j]];
+			maxChildIndex=child[j];
+		}
+	}
+	if(maxChild== -1)
+		break;
+	if(array[maxChildIndex]>array[i]){
+		swap(array,maxChildIndex,i);
+	}
+	i=maxChildIndex;
+}	
+}
+void buildHeap(int *array,int n,int k){
+	int i;
+	printf("hi");
+	for(i=(n-1)/k;i>=0;i--){
+		printf("in buildheap %d",i);
+		restoreDown(array,n,k,i);
+	}
+}
 int main(){
-	int n, *array,k;
+	int n, *array,k,i;
 	scanf("%d%d",&n,&k);
 	array=(int *)malloc(n*sizeof(int));
-	for(i=0;i<n;i++)
+//	printf("hi");
+
+	for(i=0;i<n;i++){
 		scanf("%d",&array[i]);
-	maxHeap(array,n,k);
+	}
+//	printf("hi");
+//	 for(i=0;i<n;i++){
+  //            printf("%d\t",array[i]);
+ //     }
 
+	buildHeap(array,n,k);
+	for(i=0;i<n;i++){
+		printf("%d\t",array[i]);
+	}
 
-return 0;
+	return 0;
 }
